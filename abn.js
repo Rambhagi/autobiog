9c917871bc9da9af8afb052a783882da62c022d3
Rambhagi <bhargavibakuru@tucows.co.in>
2023-12-06 11:23:23 +0530

    first commit

---
+++ abn.js
@@ -0,0 +1,20 @@
+function isAutoBio(number) {
+    const numberStr = String(number);
+    for (let i=0; i < numberStr.length; i++){
+        const digit = Number(numberStr[i]);
+        const count = numberStr.split(String(i)).length -1;
+        if (count !== digit) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+console.log(isAutoBio(1210));  
+
+console.log(isAutoBio(1220));
+
+console.log(isAutoBio(2020));
+
+
